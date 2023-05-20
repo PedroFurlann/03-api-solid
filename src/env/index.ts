244512ee33c62dbacfa598462a8cd8b92e2834ba
Pedro Furlan <pedrofurlan6@gmail.com>
2023-05-20 11:25:59 -0300

    Create controllers folder and register controller

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3335),
+  PORT: z.coerce.number().default(3339),
 })
 
 const _env = envSchema.safeParse(process.env)
