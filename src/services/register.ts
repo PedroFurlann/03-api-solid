bbe4147a096d30ca6be08ed47df406aad6c25694
Pedro Furlan <pedrofurlan6@gmail.com>
2023-05-21 12:11:47 -0300

    Create firts unit test for Register Service

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,16 +1,26 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '../repositories/users-repository'
 import { UserAlreadyTakenError } from './errors/user-already-taken-error'
+import { User } from '@prisma/client'
 
 interface RegisterParams {
   name: string
   email: string
   password: string
 }
+
+interface RegisterServiceResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async registerUser({ name, email, password }: RegisterParams) {
+  async registerUser({
+    name,
+    email,
+    password,
+  }: RegisterParams): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userAlreadyTakenThisEmail = await this.usersRepository.findByEmail(
@@ -21,10 +31,12 @@ export class RegisterUseCase {
       throw new UserAlreadyTakenError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
